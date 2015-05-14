0e892de88a36ecd9c4e320982c5ff0feb271eab5
William Petit <wpetit@cadoles.com>
2015-05-14 19:50:24 +0200

    Editor auto install + module injection

--- scripts/install-editor.sh
+++ scripts/install-editor.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+rm -rf web/editor
+
 editor_path="$(readlink -e vendor/snap-project/app-builder)"
-ln -s ${editor_path} web/editor
+editor_deps_override_path="$(readlink -e web/bundles/kezacoeditor/js/app-builder-deps.js)"
+angular_module_path="$(readlink -e  web/bundles/kezacoeditor/js/editor)"
+
+# Expose editor
+ln -fs ${editor_path} web/editor
+
+# Override editor deps
+ln -fs ${editor_deps_override_path} web/editor/js/deps.js
+ln -fs ${angular_module_path} web/editor/js/kezaco
+
+# Install editor deps
 cd web/editor
 bower install
---
+++ src/Kezaco/EditorBundle/Resources/public/js/app-builder-deps.js
@@ -0,0 +1,12 @@
+/* jshint node:true */
+
+// Application dependencies
+require('angular-gridster/dist/angular-gridster.min');
+require('angular-route/angular-route');
+require('./kezaco/kezaco-editor');
+
+module.exports = [
+  'ngRoute',
+  'gridster',
+  'KezacoEditor'
+];
