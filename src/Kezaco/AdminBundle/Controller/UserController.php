f773a09a44e173b64dad688609810d180c36c849
FoxDeveloper <development@gaudebenjamin.com>
2015-05-16 17:46:30 +0200

    Ajout suppression utilisateur

--- src/Kezaco/AdminBundle/Controller/UserController.php
+++ src/Kezaco/AdminBundle/Controller/UserController.php
@@ -5,7 +5,8 @@ namespace Kezaco\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Kezaco\CoreBundle\Entity\User;
 
 class UserController extends Controller
@@ -25,10 +26,38 @@ class UserController extends Controller
         ]);
     }
 
-    public function deleteAction(User $user)
+    /**
+     * @Route("admin/users/delete/{id}", name="kezaco_admin_user_delete")
+     * @Template()
+     *
+     * @param integer  $id      ID's user
+     * @param Request $request  Request object
+     */
+    public function deleteAction($id, Request $request)
     {
-      $form = $this->createFormDelete($user);
-      
+      $em = $this->getDoctrine()->getManager();
+
+      $user = $em->getRepository('KezacoCoreBundle:User')->findOneBy(['id' => $id]);
+
+      if (null === $user) {
+        throw new NotFoundHttpException("L'utilisateur d'id ".$id." n'existe pas.");
+      }
+
+      $form = $this->createFormBuilder()->getForm();
+
+      if ($form->handleRequest($request)->isValid()) {
+        $em->remove($user);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('info', "L'utilisateur a bien été supprimé.");
+
+        return $this->redirect($this->generateUrl('kezaco_admin_user_index'));
+      }
+
+      return $this->render('KezacoAdminBundle:User:delete.html.twig', [
+        'user' => $user,
+        'form' => $form->createView()
+        ]);
     }
 
 }
