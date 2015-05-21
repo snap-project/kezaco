fa2c6d286f8a74a72467422f91dfe28cd488506f
William Petit <william.petit@ptitcloud.fr>
2015-05-21 22:12:33 +0200

    Ajout test logo + navbar

--- src/Kezaco/CoreBundle/Controller/DefaultController.php
+++ src/Kezaco/CoreBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="kezaco_home")
      * @Template()
      */
     public function indexAction()
