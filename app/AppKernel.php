01aabe7a046e97058383aff54e6d38d3dafc513f
William Petit <william.petit@ptitcloud.fr>
2015-06-16 20:09:53 +0200

    Ajjout bundle FOSElastica + configuration conteneur ElasticSearch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,9 +23,9 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Voryx\RESTGeneratorBundle\VoryxRESTGeneratorBundle(),
-            new Lexik\Bundle\FixturesMapperBundle\LexikFixturesMapperBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
+
             new Kezaco\CoreBundle\KezacoCoreBundle(),
             //new Kezaco\EditorBundle\KezacoEditorBundle(),
             new Kezaco\AdminBundle\KezacoAdminBundle(),
