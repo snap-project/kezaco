1cc9ba3537c866ccb9a1eb1a7a92496fa0ffdf96
William Petit <william.petit@ptitcloud.fr>
2015-06-16 23:42:15 +0200

    Base ResourceController

---
+++ src/Kezaco/CoreBundle/Form/ResourceType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kezaco\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ResourceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('description')
+            ->add('creationDate')
+            ->add('lastUpdate')
+            ->add('author')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Kezaco\CoreBundle\Entity\Resource'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'kezaco_corebundle_resource';
+    }
+}
